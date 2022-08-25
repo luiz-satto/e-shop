5581aa5d5ac119ab20b4fa6ac3408e94ede1a6c6
Luiz Satto <luizf.satto@gmail.com>
2022-08-25 17:54:08 -0300

    Adding product and some files

---
+++ e-shop.Application/Products/SearchProduct.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using e_shop.Core.Models;
+
+namespace e_shop.Application.Search
+{
+    public class SearchProduct
+    {
+        public IEnumerable<Product> Execute(string filter)
+        {
+            return null;
+        }
+    }
+}
---
+++ e-shop.Core/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace e_shop.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ImageLink { get; set; }
+    }
+}
